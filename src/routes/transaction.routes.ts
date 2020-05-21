17d6bc8a82d9000fc7976698bfd02ba1d181c6c4
joaopedroassis <joaopedroassisdossantos@gmail.com>
2020-05-21 03:09:04 -0300

    return all transactions

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -7,13 +7,15 @@ const transactionRouter = Router();
 
 const transactionsRepository = new TransactionsRepository();
 
-// transactionRouter.get('/', (request, response) => {
-//   try {
-//     // TODO
-//   } catch (err) {
-//     return response.status(400).json({ error: err.message });
-//   }
-// });
+transactionRouter.get('/', (request, response) => {
+  try {
+    const transactions = transactionsRepository.all();
+
+    return response.json(transactions);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
 
 transactionRouter.post('/', (request, response) => {
   const { title, value, type } = request.body;
